49aad36645bfe20a4b418eff0a5b75fdc021a9fb
Ebru Rice <ebruerenb@gmail.com>
2022-02-26 20:12:16 -0800

    Update everything to match Epicodus repo

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,35 +1,20 @@
-using System;
-using System.Collections.Generic;
-using ToDoList.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-namespace ToDoList {
-
+namespace ToDoList
+{
   public class Program
   {
-    public static void Main()
+    public static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to the To Do List");
-      Console.WriteLine("Would you like to add an item to your list or view your list? [Add/View]");
-      string answer = Console.ReadLine();
-      if (answer == "Add" || answer == "add")
-      {
-        Console.WriteLine("Add your item");
-        string item = Console.ReadLine();
-        Item newItem = new Item(item);
-        Main();
-      }
-      else if (answer == "View" || answer == "view")
-      {
-        foreach (Item task in Item.GetAll())
-        {
-        Console.WriteLine(task.Description);
-        }
-        Main();
-      }
-      else
-      {
-        Console.WriteLine("PLEASE READ: Would you like to add an item to your list or view your list? [Add/View]");
-      }
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
     }
   }
 }
